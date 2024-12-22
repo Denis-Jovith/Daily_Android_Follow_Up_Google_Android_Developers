96e65e6f710e17484a2f1f6dcdb07ef5953984f8
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2024-12-22 20:40:47 +0300

    Learning Button Composable

--- app/src/main/java/com/example/daily_android_follow_up_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/daily_android_follow_up_kotlin/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.daily_android_follow_up_kotlin.ui.theme.Daily_Android_Follow_Up_KotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +22,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Daily_Android_Follow_Up_KotlinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+
+                ){
+                    Timely()
                 }
             }
         }
@@ -31,17 +33,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Timely(){
+
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
+@Preview(showBackground = true)
+fun TimelyPreview() {
     Daily_Android_Follow_Up_KotlinTheme {
-        Greeting("Android")
+        Timely()
     }
 }
